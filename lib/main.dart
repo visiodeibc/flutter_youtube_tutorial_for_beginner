f6c533c2d459742a8d7e2a6bb1ed4a042c0dbc66
visiodeibc <ljh95714@gmail.com>
2019-07-27 23:53:55 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// tutorial link: www.youtube.comwatchv=GLSG_Wh_YWc&t=8440s
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+      ),
+    );
+  }
+}
