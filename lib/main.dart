f287966a9c90048a579984b6a6eedd2e4a90c8d4
visiodeibc <ljh95714@gmail.com>
2019-07-28 23:08:28 +0800

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = [
+    'Food Tester',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _products.add('value');
+                  });
+                  print(_products);
+                },
+                child: Text('add product'),
+              ),
+            ),
+            Column(
+              children: _products
+                  .map((element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/pic1.jpg'),
+                            Text(element)
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ],
         ),
       ),
     );
