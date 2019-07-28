e524d445a2af0214956d478b0888faa4ad29ceb7
visiodeibc <ljh95714@gmail.com>
2019-07-28 23:39:22 +0800

    class separation

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
 
 // tutorial link: www.youtube.comwatchv=GLSG_Wh_YWc&t=8440s
-
+import 'product_manager.dart';
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = [
-    'Food Tester',
-  ];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,34 +12,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('value');
-                  });
-                  print(_products);
-                },
-                child: Text('add product'),
-              ),
-            ),
-            Column(
-              children: _products
-                  .map((element) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assets/pic1.jpg'),
-                            Text(element)
-                          ],
-                        ),
-                      ))
-                  .toList(),
-            ),
-          ],
-        ),
+        body:ProductManager(),
       ),
     );
   }
---
+++ lib/product_manager.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+
+class ProductManager extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [
+    'Food Tester',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Column(children: [Container(
+      margin: EdgeInsets.all(10.0),
+      child: RaisedButton(
+        onPressed: () {
+          setState(() {
+            _products.add('value');
+          });
+          print(_products);
+        },
+        child: Text('add product'),
+      ),
+    ),
+    Products(_products)
+    ],);
+  }
+}
